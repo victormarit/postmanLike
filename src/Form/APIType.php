9b1352a286258b9d0ae84c297982e3c491746fea
victormarit <victormarit.95@gmail.com>
2020-11-02 15:31:46 +0100

    formulaire API

---
+++ src/Form/APIType.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\API;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class APIType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('name', TextType::class, [
+                "attr" => [
+                    "class" => "form-control"
+                ]
+            ])
+            ->add('description', TextareaType::class, [
+                "attr" => [
+                    "class" => "form-control"
+                ]
+            ])
+            ->add('url', TextType::class, [
+                "attr" => [
+                    "class" => "form-control"
+                ]
+            ])
+            ->add('methode', TextType::class, [
+                "attr" => [
+                    "class" => "form-control"
+                ]
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => API::class,
+        ]);
+    }
+}
